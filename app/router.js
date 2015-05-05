22ef08df96a1def3baed2d3c4dde46656be2c082
hzzcc <hzz.gofur@gmail.com>
2015-05-05 23:41:34 +0800

    login

--- app/router.js
+++ app/router.js
@@ -6,4 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+
+  this.resource('home', { path: "/"});
+
+  this.resource('profile', function () {
+    this.route('login');
+    this.route('register');
+    this.route('forgetPassword');
+    this.route('forgetVerifiedSuccess');
+    this.route('resetPassword');
+  });
+
 });
---
+++ app/views/application.js
@@ -0,0 +1,6 @@
+export default Em.View.extend({
+    didInsertElement: function() {
+      var instance1 = new gmu.Toolbar('#J_toolbar');
+      var instance2 = new gmu.Navigator('#nav');
+    }
+});
