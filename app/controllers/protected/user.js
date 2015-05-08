6b07cf5a27811d78a635336cdbcceca64ee29a82
hzzcc <hzz.gofur@gmail.com>
2015-05-08 14:46:02 +0800

    个人中心toolbar

--- app/controllers/protected/user.js
+++ app/controllers/protected/user.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  toolbar_name: "当前用户",
-  toolbar_back_show: false
+  toolbar_name: "个人中心",
+  toolbar_back_show: false,
+  toolbar_logout: true
 });
