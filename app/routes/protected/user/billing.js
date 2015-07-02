77bbb2f9e33f7afa6b8a0b93e4c7842c06fcf4ed
hzzcc <hzz.gofur@gmail.com>
2015-07-02 16:11:56 +0800

    fix  css

--- app/routes/protected/user/billing.js
+++ app/routes/protected/user/billing.js
@@ -3,7 +3,7 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
 export default Ember.Route.extend(RouteMixin, {
   model: function(params) {
-      // return this.findPaged('billing',params);
-      return {};
+      return this.findPaged('billing',params);
+      // return {};
   }
 });
